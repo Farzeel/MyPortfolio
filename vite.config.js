a2d948c025fed33904afb966c36253cd1a386fb8
Farzeel <farzeelaftab544@gmail.com>
2025-05-08 22:28:25 +0200

    version9

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/MyPortfolio/",
+  base: "/",
   
   plugins: [react(), tailwindcss()],
   resolve: {
