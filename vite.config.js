44adf822d36193d6ad9e40e86b72a90c51dba6cc
Farzeel <farzeelaftab544@gmail.com>
2025-05-08 21:25:26 +0200

    version 4

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    host:"0.0.0.0",
-    port:5173,
-    allowedHosts: [
-      'shark-mint-blindly.ngrok-free.app',  // your ngrok domain here
-    ],
-  },
+  base: "/MyPortfolio",
+  
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
